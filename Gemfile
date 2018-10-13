3cf2aa3c15549e271f2379b1ab1311b2024b42f2
Амир Хасанов <aworldx@gmail.com>
2018-10-13 20:13:25 +0300

    finish user sign up

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'slim'
 gem 'simple_form'
 gem 'ice_cube', '~> 0.14.0'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_143503) do
+ActiveRecord::Schema.define(version: 2018_10_13_170951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2018_10_13_143503) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.string "password_diggest"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
